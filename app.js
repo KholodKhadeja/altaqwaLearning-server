671fc51a735ea09dfadb731104563782a07eef0f
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-01-20 15:26:26 +0200

    server update

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const apiRouter = require("./routes/api/api");
 
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -16,7 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api", apiRouter);
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 module.exports = app;
